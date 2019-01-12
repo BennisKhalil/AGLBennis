8f8853c52d3c15583178913b06cd038c6e73d8a8
benniskhalil <khalil.bennis.info@gmail.com>
2019-01-12 20:26:40 +0100

    lol

--- src/main/java/sample/Main.java
+++ src/main/java/sample/Main.java
@@ -4,7 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.GridPane;
-import javafx.stage.Stage;
+import javafx.stage.Stage;;
 
 import java.net.URL;
 
